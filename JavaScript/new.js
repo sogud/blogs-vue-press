b596739b2eb3c43d8cdede33e0874b9c429f8b1f
shunshun <lwwtotod@gmail.com>
2019-03-20 21:08:08 +0800

    new实现原理

--- JavaScript/new.js
+++ JavaScript/new.js
@@ -1,3 +1,8 @@
+/**
+ * 理解new的运算过程
+ * @param {*} name
+ *
+ */
 function Person(name) {
   this.name = name
 }
@@ -8,16 +13,21 @@ Person.prototype.getName = function() {
 
 var objectFactory = function() {
   console.log(arguments)
+  //声明一个空对象
   var obj = new Object()
+  // 截取参数第一个赋值给Constructor
   var Constructor = [].shift.call(arguments)
+  // 将obj的原型链指向Constructor的原型
   obj.__proto__ = Constructor.prototype
   console.log(obj.__proto__)
   console.log(arguments)
+  // 将Constructor的作用域指向obj
   var ret = Constructor.apply(obj, arguments)
 
   return typeof ret === 'object' ? ret : obj
 }
 
 var a = objectFactory(Person, 'sven')
-
+var b = new Person('sven')
+console.log(b.getName())
 console.log(a.getName())
