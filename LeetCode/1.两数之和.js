8052d74c17d489de804c7ddb0c38d78400a9b867
shunshun <lwwtotod@gmail.com>
2019-04-10 14:51:01 +0800

    chore

---
+++ LeetCode/1.两数之和.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[j] = target - nums[i]) {
+        return [i, j]
+      }
+    }
+  }
+};
