baef97b0f5be198f8aa213cd954f80ecc7639d94
shunshun <lwwtotod@gmail.com>
2020-02-21 22:23:05 +0800

    fix:test
    Update main.yml
    
    Update main.yml
    
    Update main.yml
    
    Update main.yml
    
    Update main.yml
    
    auto-deploy
    
    Update main.yml
    
    Update main.yml
    
    Update main.yml
    
    Update main.yml
    
    chore:build related
    
    chore:travis
    
    fix

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,31 +1,45 @@
 module.exports = {
-  // theme: 'vuepress-theme-minimalism',
   base: '/',
   title: '记录',
-  description: 'description...',
+  theme: 'vuepress-theme-minimalism',
   themeConfig: {
-    home: true,
-    actionText: '开始Start reading...',
-    actionLink: '/list',
+    headerText: {
+      index: 'ホーム',
+      all: '一覧',
+      tags: 'カテゴリー',
+      timeline: 'タイムライン',
+      seacrh: '探す'
+    },
+    footerText:
+      '©2020 Power by <a href="https://vuepress.vuejs.org">VuePress</a> <a href="https://github.com/onnezezt/vuepress-theme-minimalism">Theme minimalism</a>',
     homeBackground: {
       //设置主页背景颜色，false title为默认颜色
-      show: true,
-      fileName: '/GetImage.jpeg'
+      show: false,
+      fileName: '/bg.png'
     },
     PostsListPopover: false, //是否显示文章内容提示
     darkMode: {
       //暗模式配置
       switch: true, //开关
       auto: false, //自动开启
-      on: '', //时间
-      off: ''
+      on: '18', //时间
+      off: '6'
     },
     vssue: {
+      //开启vssue评论
+      use: false,
       option: {
         owner: 'onnezezt',
         repo: 'onnezezt.github.io',
-        clientId: '3f78ce485d06bd79d223',
-        clientSecret: 'bf0795a8364c2e403d6a377b636384bf92a81385' // 只有在使用某些平台时需要
+        clientId: 'xxxx',
+        clientSecret: 'xxxx' // 只有在使用某些平台时需要
+      }
+    },
+    disqus: {
+      //开启disqus评论
+      use: true,
+      option: {
+        name: '1net-1' // Replace PAGE_URL with your page's canonical URL variable
       }
     },
     nav: [
--- docs/index.md
+++
@@ -1,6 +0,0 @@
----
-home: true
-actionText: 开始Start reading...
-actionLink: /list
-# footer: ©2020 Power by VuePress Theme minimalism
----
